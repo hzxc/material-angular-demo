311d4ff51031af23579abcd231c463893e44a40b
hzxc <hzxc@LK1028>
2017-12-25 17:18:51 +0800

    t

--- src/app/other/timeago-demo/jq-plugin.directive.ts
+++ src/app/other/timeago-demo/jq-plugin.directive.ts
@@ -1,22 +1,41 @@
-import { Directive, ElementRef, AfterViewInit } from '@angular/core';
+import { Directive, ElementRef, AfterViewInit, OnInit } from '@angular/core';
+import { AfterViewChecked, OnChanges } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Directive({
   // tslint:disable-next-line:directive-selector
   selector: '[jq-plugin]'
 })
-export class JqPluginDirective implements AfterViewInit {
+export class JqPluginDirective implements OnInit, OnChanges, AfterViewInit, AfterViewChecked {
+
+
+
 
   constructor(
     private _element: ElementRef
   ) {
+
+  }
+
+  ngOnInit() {
+
+  }
+
+  ngOnChanges() {
+
+  }
+
+  ngAfterViewChecked() {
+
   }
 
   ngAfterViewInit(): void {
+    console.log('ngAfterViewChecked');
     const $element = $(this._element.nativeElement);
     const pluginName = $element.attr('jq-plugin');
     const options = $element.attr('jq-options');
     if (!options) {
-      $element[pluginName]();
+      $element.timeago();
+      // $element[pluginName]();
     } else {
       // tslint:disable-next-line:no-eval
       $element[pluginName](eval('(' + options + ')'));
